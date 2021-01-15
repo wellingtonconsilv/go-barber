b7f7b0a2f1c4eda15482aeb1fadc63db836f1329
wellingtonconsilv <wellingtonconsilv@gmail.com>
2021-01-15 15:25:59 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    // 'no-unsed-vars': ['error', { argsIgnorePattern: ['next'] }],
+  },
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '797185f47fff229671e44162efd20e70',
+  expireIn: '7d',
+};
